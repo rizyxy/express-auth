a59256a2c36e13f72396543787d4bee15413f9b5
Mukhammad Rizki <rizkyRE7@gmail.com>
2026-02-24 21:24:59 +0700

    Initialized Express Auth Scaffolding

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express"
+
+const AuthController = {
+    async login(req: Request, res: Response) {
+
+    },
+
+    async register(req: Request, res: Response) {
+
+    }
+}
